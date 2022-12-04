94aec65a40a4533b09e2dc9cf1d38ece2e4a4e8a
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2022-12-04 12:42:09 +0100

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,33 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
+// MQTT callback function called when a publish message is received
 static void mqtt_callback(char *topic, byte *payload, unsigned int length);
+// Helper function used to reconnect to MQTT server when connection is lost
 static void mqtt_reconnect(void);
 
 // Static IP address to use in case the DHCP client fails to get an address
-IPAddress staticIP(192, 168, 1, 55);
-EthernetClient ethernetClient;
-PubSubClient mqttClient(ethernetClient);
-StaticJsonDocument<200> jsonDoc;
-bool ledIsOn;
-
+const IPAddress staticIP(192, 168, 1, 55);
+// MQTT configs
 const uint16_t mqttServerPort = 1883;
 const char *mqttServerHostname = "test.mosquitto.org";
 const char *mqttSubscribeTopic = "gdg/test";
+// Bool used to track the LED state
+bool ledIsOn;
+// TCP client object to be used by the MQTT client
+EthernetClient ethernetClient;
+// MQTT client object
+PubSubClient mqttClient(ethernetClient);
+// Static JSON document object to parse the received message over MQTT
+StaticJsonDocument<200> jsonDoc;
 
 void setup() {
+  // Initialize the LED
   ledIsOn = false;
   pinMode(LED_BLUE, OUTPUT);
+  // Initialize the serial port
   Serial.begin(115200);
-  // Ethernet setup
+  // Setup ethernet with DHCP else use a static IP
   if(Ethernet.begin() == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     Serial.print("Configuring Ethernet using static IP: ");
@@ -32,16 +40,18 @@ void setup() {
     Serial.println("Ethernet is configured successfully using DHCP");
   }
   Serial.print("Connected to network. IP = ");
-  // MQTT setup
   Serial.println(Ethernet.localIP());
+  // Setup MQTT configs
   mqttClient.setServer(mqttServerHostname, mqttServerPort);
   mqttClient.setCallback(mqtt_callback);
 }
 
 void loop() {
+  // Check if client is disconnected, if so reconnect
   if(!mqttClient.connected()) {
     mqtt_reconnect();
   }
+  // Process incoming MQTT packets
   mqttClient.loop();
 }
 
@@ -56,20 +66,22 @@ static void mqtt_callback(char *topic, byte *payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
   // Parse the received JSON message and take action accordingly
   error = deserializeJson(jsonDoc, payload);
   if(0 == error) {
     Serial.println("JSON message parsed successfully");
     if(jsonDoc["lights_state"] == "on") {
+      // Turn on LED
       digitalWrite(LED_BLUE, HIGH);
       ledIsOn = true;
     }
     else if(jsonDoc["lights_state"] == "off") {
+      // Turn off LED
       digitalWrite(LED_BLUE, LOW);
       ledIsOn = false;
     }
     else if(jsonDoc["lights_state"] == "toggle") {
+      // Toggle LED and update its state
       digitalWrite(LED_BLUE, (true == ledIsOn) ? LOW : HIGH);
       ledIsOn = (true == ledIsOn) ? false : true;
     } else {
@@ -90,15 +102,16 @@ static void mqtt_reconnect(void) {
       Serial.println(mqttServerHostname);
       // Once connected, subscribe
       if(mqttClient.subscribe(mqttSubscribeTopic)) {
-        Serial.print("Subscribed to ");
-        Serial.println(mqttSubscribeTopic);
+        Serial.print("Subscribed to \"");
+        Serial.print(mqttSubscribeTopic);
+        Serial.println("\"");
       } else {
         Serial.println("Failed to subscribe");
       }
     } else {
       Serial.print("Failed to connect, rc=");
       Serial.print(mqttClient.state());
-      Serial.println(" try again to connect in 5 seconds");
+      Serial.println(" trying to connect again in 5 seconds...");
       // Wait 5 seconds before retrying
       delay(5000);
     }
